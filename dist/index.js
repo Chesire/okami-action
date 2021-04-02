5d54dd79a1e73dfb045b7b7a290f3854810c8b98
Chesire <troyrijkaard@hotmail.co.uk>
2021-04-02 21:57:36 +0100

    feat: integrate more of the github api in

--- dist/index.js
+++ dist/index.js
@@ -8,35 +8,41 @@ module.exports =
 const core = __nccwpck_require__(698);
 const github = __nccwpck_require__(695);
 
+async function run() {
+    if (context.payload.pull_request == null) {
+        core.setFailed('No pull request found.');
+        return;
+    }
+    
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const githubToken = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(githubToken)
+    const prNumber = context.payload.pull_request.number
+    
+    console.log(pullRequest);
+
+    // octokit.rest.pulls.createReviewComment({
+    octokit.pulls.createReviewComment({
+        //...context.owner,
+        ...context.repo,
+        pull_number: prNumber,
+        body: "Test",
+    });
+}
+
+run();
+
+/*
 try {
-  const context = github.context;
-  core.debug("Info dump")
-  core.debug(github.context)
-  core.debug(github.context.eventName)
-  core.debug(github.context.action)
-  core.debug(github.context.payload)
-  core.debug(github.context.pull_request)
-  core.debug(github.context.pull_request_number)
 
-  if (context.payload.pull_request == null) {
-      core.setFailed('No pull request found.');
-      return;
-  }
-
-  const github_token = core.getInput('GITHUB_TOKEN');
-  const octokit = new github.GitHub(github_token);
-  
-  const pull_request_number = context.payload.pull_request.number;
-
-  // const octokit = github.getOctokit(github_token)
-  const new_comment = octokit.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request_number,
-    body: "Test input"
-  });
+};
 } catch (error) {
   core.setFailed(error.message);
 }
+*/
 
 
 /***/ }),
