1ac78294cf8c6d083e4cc16f954c5de162919a36
Chesire <troyrijkaard@hotmail.co.uk>
2021-04-02 23:05:26 +0100

    feat: begin api request

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ module.exports =
 
 const core = __nccwpck_require__(698);
 const github = __nccwpck_require__(695);
+const https = __nccwpck_require__(211);
 
 async function run() {
     const context = github.context;
@@ -15,18 +16,48 @@ async function run() {
         return;
     }
 
-// github.token ?
     const token = core.getInput('okami-token');
     const octokit = github.getOctokit(token);
     const prNumber = context.payload.pull_request.number;
     
+    const picture = getShibaPicture()
+
     octokit.issues.createComment({
         ...context.repo,
         issue_number: prNumber,
         body: "Test"
-      });
+    });
 }
 
+async function getShibaPicture() {
+    var result = '';
+    const options = {
+        hostname: 'shibe.online',
+        path: '/api/shibes',
+        method: 'GET'
+    }
+// http://shibe.online/api/shibes?urls=[true/false]
+
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            result += d
+        })
+        res.on('error', error => {
+            console.log(`error occurred: ${error}`)
+        })
+        res.on('end', () => {
+            console.log(`ended ${result}`)
+        })
+    })
+
+    req.end()
+    return result
+}
+
+
+// /api/cats or birds at /api/birds
 run();
 
 /*
